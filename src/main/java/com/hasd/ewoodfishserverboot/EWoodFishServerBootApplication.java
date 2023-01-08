d6910a5b26a5b0be0a334cc0377446f3561c4845
hasdsd <1259305706@qq.com>
2023-01-08 14:16:16 +0800

    first commit

---
+++ src/main/java/com/hasd/ewoodfishserverboot/EWoodFishServerBootApplication.java
@@ -0,0 +1,13 @@
+package com.hasd.ewoodfishserverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWoodFishServerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EWoodFishServerBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasd/ewoodfishserverboot/EWoodFishServerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasd.ewoodfishserverboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWoodFishServerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
